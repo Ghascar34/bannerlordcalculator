7757c551c6f655eee29b51956d732c4c129974ff
Ghascar34 <86572845+Ghascar34@users.noreply.github.com>
2021-06-29 15:20:25 +0300

    Update Bannerlord Calculator By Ghascar.py

--- Bannerlord Calculator By Ghascar.py
+++ Bannerlord Calculator By Ghascar.py
@@ -1,114 +1,376 @@
 import os
 
 savascilar = {
-    "KhuzaitNomad": 112,
-    "KhuzaitTribalWarrior": 321,                                                                                                                      
-    "KhuzaitRaider": 780,
-    "KhuzaitHorseArcher": 1736,
-    "KhuzaitHeavyHorseArcher": 2884,
-    "KhuzaitHorseMan": 780,
-    "KhuzaitLancer": 1510,
-    "KhuzaitHeavyLancer": 2553,
-    "KhuzaitFootMan": 321,
-    "KhuzaitHunter" : 738,
-    "KhuzaitArcher" : 1505,
-    "KhuzaitMarksman" : 2600,
-    "KhuzaitSpearman" : 738,
-    "KhuzaitSpearInfantry" : 1468,
-    "KhuzaitDarkhan" : 2511,
-    "VlandianRecruit": 80,
-    "VlandianLevyCrossbowman": 280,
-    "VlandianCrossbowman": 699,
-    "VlandianHardenedCrossbowman": 1432,
-    "VlandianSharpshooter": 2729,
-    "VlandianFootman": 280,
-    "VlandianSpearman": 679,
-    "VlandianBillman": 1378,
-    "VlandianVoulgier": 2376,
-    "VlandianPikeman" : 2376,
-    "VlandianInfantry" : 679,
-    "VlandianLightCavalry": 1378,
-    "VlandianVanguard" : 2376,
-    "VlandianSwordsman": 1378,
-    "VlandianSergeant" : 2077,
-
-
-
+    "KhuzaitNomad": 112 ,
+    "KhuzaitTribalWarrior": 321 ,                                                                                                                      
+    "KhuzaitRaider": 780 ,
+    "KhuzaitHorseArcher": 1736 ,
+    "KhuzaitHeavyHorseArcher": 2884 ,
+    "KhuzaitHorseMan": 780 ,
+    "KhuzaitLancer": 1510 ,
+    "KhuzaitHeavyLancer": 2553 ,
+    "KhuzaitFootMan": 321 ,
+    "KhuzaitHunter" : 738 ,
+    "KhuzaitArcher" : 1505 ,
+    "KhuzaitMarksman" : 2600 ,
+    "KhuzaitSpearman" : 738 ,
+    "KhuzaitSpearInfantry" : 1468 ,
+    "KhuzaitDarkhan" : 2511 ,
+    "VlandianRecruit": 80 ,
+    "VlandianLevyCrossbowman": 280 ,
+    "VlandianCrossbowman": 699 ,
+    "VlandianHardenedCrossbowman": 1432 ,
+    "VlandianSharpshooter": 2729 ,
+    "VlandianFootman": 280 ,
+    "VlandianSpearman": 679 ,
+    "VlandianBillman": 1378 ,
+    "VlandianVoulgier": 2376 ,
+    "VlandianPikeman" : 2376 ,
+    "VlandianInfantry" : 679 ,
+    "VlandianLightCavalry": 1378 ,
+    "VlandianVanguard" : 2376 ,
+    "VlandianSwordsman": 1378 ,
+    "VlandianSergeant" : 2077 ,
+    "SturgianRecruit" : 80 ,
+    "SturgianWarrior" : 280 ,
+    "SturgianSoldier" : 679 ,
+    "SturgianSpearman" : 1378 ,
+    "SturgianShockTroop" : 2376 ,
+    "SturgianVeteranWarrior" : 2376 ,
+    "SturgianBerserker" : 1378 ,
+    "SturgianUlfhednar" : 2376 ,
+    "SturgianWoodsman" : 280 ,
+    "SturgianBrigand" : 679 ,
+    "SturgianHunter" : 679 ,
+    "SturgianHardenedBrigand": 1444 ,
+    "SturgianHorseRaider" : 2442 ,
+    "SturgianArcher": 1432 ,
+    "SturgianVeteranBowman" : 2340 ,
+    "AseraiRecruit": 80 ,
+    "AseraiTribesman" : 280 ,
+    "AseraiSkirmisher" :679 ,
+    "AseraiArcher" : 1378 ,
+    "AseraiMasterArcher" : 2426 ,
+    "AseraiFootman" : 679 ,
+    "AseraiInfantry" : 1378 ,
+    "AseraiVeteranInfantry" : 2376 ,
+    "AseraiMamelukeSoldier" : 280 ,
+    "AseraiMamelukeRegular" : 679 ,
+    "AseraiMamelukeCavalry" : 1378 ,
+    "AseraiMamelukeHeavyCavalry" : 2409 ,
+    "AseraiMamelukeAxeman" : 679 ,
+    "AseraiMamelukeGuard" : 1411 ,
+    "AseraiMamelukePalaceGuard" : 2409,
+    "ImperialRecruit" : 80 ,
+    "ImperialInfantryMan" : 280 ,
+    "ImperialTrainedInfantryMan" : 679 ,
+    "ImperialVeteranInfantryMan" : 1378 ,
+    "ImperialLegionary" : 2376 ,
+    "ImperialMenavliaton" : 1378 ,
+    "ImperialEliteMenavliaton" : 2376 ,
+    "ImperialArcher" : 280 ,
+    "ImperialTrainedArcher" : 699 ,
+    "ImperialVeteranArcher" : 1432 ,
+    "ImperialPalatineGuard" : 2480 ,
+    "ImperialBucellarii" : 2480 ,
+    "ImperialCrossbowman" : 1432 ,
+    "ImperialSergeantCrossbowman" : 2480 ,
+    "BattanianVolunteer" : 80 ,
+    "BattanianClanwarrior" : 280 ,
+    "BattanianTrainedWarrior" : 679 ,
+    "BattanianPickedWarrior" : 1378 ,
+    "BattanianOathsworn" : 2442 ,
+    "BattanianWoodrunner" : 280 ,
+    "BattanianRaider" : 679 ,
+    "BattanianSkirmisher" : 679 ,
+    "BattanianFalxman" : 1378 ,
+    "BattanianVeteranFalxman" : 2376 ,
+    "BattanianVeteranSkirmisher" : 1311 ,
+    "BattanianWildling" : 2276 ,
+    "BattanianMountedSkirmisher" : 2276 ,
+    "BattanianScout" : 1378 , 
+    "BattanianHorseman" : 2376
 
      }
 def getPrice(savasci,x):
    os.system("cls")
    print(savascilar[savasci]*x, "Dinar")
    main()
-   
-def sturgia():
+
+def battanian():
     metin = """
-    1) Vlandian Recruit
-    2) Vlandian Levy Crossbowman
-    3) Vlandian Crossbowman
-    4) Vlandian Hardened Crossbowman
-    5) Vlandian Sharpshooter
-    6) Vlandian Footman
-    7) Vlandian Spearman
-    8) Vlandian Billman
-    9) Vlandian Voulgier
-    10) Vlandian Pikeman
-    11) Vlandian Infantry
-    12) Vlandian Light Cavalary
-    13) Vlandian Vanguard
-    14) Vlandian Swordsman
-    15) Vlandian Sergeant
+    1) Battanian Volunteer
+    2) Battanian Clan Warrior
+    3) Battanian Trained Warrior
+    4) Battanian Picked Warrior
+    5) Battanian Trained Spearman
+    6) Battanian Woodrunner
+    7) Battanian Raider
+    8) Battanian Skirmisher
+    9) Battanian Falxman
+    10) Battanian Veteran Falxman
+    11) Battanian Veteran Skirmisher
+    12) Battanian Wildling
+    13) Battanian Mounted Skirmisher
+    14) Battanian Scout
+    15) Battanian Horseman
 
-    16) Ülke Değiştir
-    17) Çıkış
+    16) Change Kingdom
+    17) Exit
 
-    Mevcut Asker Rütbesi Nedir? """
+    Which one do you want?: """
 
     
     try:
         a = int(input(metin))
         if a > 17 or a < 1:
-            print("Lütfen 1-17 Arası İşlem Giriniz!")
+            print("Please enter between 1-17 only !")
         elif a == 17:
-            print("Program Sonlandırıldı!")                                                                                                                    
-        elif a == 17:
+            print("Calculator has been closed.")                                                                                                                    
+        elif a == 16:
             main()
                 
         else:
             if a == 1:
-                savasci = "VlandianRecruit"
+                savasci = "BattanianVolunteer"
             if a == 2:
-                savasci = "VlandianLevyCrossbowman"
+                savasci = "BattanianClanwarrior"
             if a == 3:
-                savasci = "VlandianCrossbowman"
+                savasci = "BattanianTrainedWarrior"
             if a == 4:
-                savasci = "VlandianHardenedCrossbowman"
+                savasci = "BattanianPickedWarrior"
             if a == 5:
-                savasci = "VlandianSharpshooter"
+                savasci = "BattanianOathsworn"
             if a == 6:
-                savasci = "VlandianFootman"
+                savasci = "BattanianWoodrunner"
             if a == 7:
-                savasci = "VlandianSpearman"
+                savasci = "BattanianRaider"
             if a == 8:
-                savasci = "VlandianBillman"
+                savasci = "BattanianSkirmisher"
             if a == 9:
-                savasci = "VlandianVoulgier"
+                savasci = "BattanianFalxman"
             if a == 10:
-                savasci = "VlandianPikeman"
+                savasci = "BattanianVeteranFalxman"
             if a == 11:
-                savasci = "VlandianInfantry"
+                savasci = "BattanianVeteranSkirmisher"
             if a == 12:
-                savasci = "VlandianLightCavalry"
+                savasci = "BattanianWildling"
             if a == 13:
-                savasci = "VlandianVanguard"
+                savasci = "BattanianMountedSkirmisher"
             if a == 14:
-                savasci = "VlandianSwordsman"
+                savasci = "BattanianScout"
             if a == 15:
-                savasci = "VlandianSergeant"
-            b = int(input("Adet Giriniz: "))
+                savasci = "BattanianHorseman"
+            b = int(input("How many?: "))
             getPrice(savasci, b)
     except ValueError:
-        print("Lütfen sadece sayı giriniz.")
+        print("Only numbers, please!")
+
+
+def imperial():
+    metin = """
+    1) Imperial Recruit
+    2) Imperial Infantryman
+    3) Imperial Trained Infantryman
+    4) Imperial Veteran Infantryman
+    5) Imperial Legionary
+    6) Imperial Menavliaton
+    7) Imperial Elite Menavliaton
+    8) Imperial Archer
+    9) Imperial Trained Archer
+    10) Imperial Veteran Archer
+    11) Imperial Palatine Guard
+    12) Imperial Bucellarii
+    13) Imperial Crossbowman
+    14) Imperial Sergeant Crossbowman
+
+    16) Change Kingdom
+    17) Exit
+
+    Which one do you want?: """
+
+    
+    try:
+        a = int(input(metin))
+        if a > 17 or a < 1:
+            print("Please enter between 1-17 only !")
+        elif a == 17:
+            print("Closed !")                                                                                                                    
+        elif a == 16:
+            main()
+                
+        else:
+            if a == 1:
+                savasci = "ImperialRecruit"
+            if a == 2:
+                savasci = "ImperialInfantryMan"
+            if a == 3:
+                savasci = "ImperialTrainedInfantryMan"
+            if a == 4:
+                savasci = "ImperialVeteranInfantryMan"
+            if a == 5:
+                savasci = "ImperialLegionary"
+            if a == 6:
+                savasci = "ImperialMenavliaton"
+            if a == 7:
+                savasci = "ImperialEliteMenavliaton"
+            if a == 8:
+                savasci = "ImperialArcher"
+            if a == 9:
+                savasci = "ImperialTrainedArcher"
+            if a == 10:
+                savasci = "ImperialVeteranArcher"
+            if a == 11:
+                savasci = "ImperialPalatineGuard"
+            if a == 12:
+                savasci = "ImperialBucellarii"
+            if a == 13:
+                savasci = "ImperialCrossbowman"
+            if a == 14:
+                savasci = "ImperialSergeantCrossbowman"
+            
+            b = int(input("How much?: "))
+            getPrice(savasci, b)
+    except ValueError:
+        print("Only numbers, please!")
+
+def aserai():
+    metin = """
+    1) Aserai Recruit
+    2) Aserai Tribesman
+    3) Aserai Skirmisher
+    4) Aserai Archer
+    5) Aserai Master Archer
+    6) Aserai Footman
+    7) Aserai Infantry
+    8) Aserai Veteran Infantry
+    9) Aserai Mameluke Soldier
+    10) Aserai Mameluke Regular
+    11) Aserai Mameluke Cavalry
+    12) Aserai Mameluke Heavy Cavalry
+    13) Aserai Mameluke Axeman
+    14) Aserai Mameluke Guard
+    15) Aserai Mameluke Palace Guard
+
+    16) Change Kingdom
+    17) Exit
+
+    Which one do you want?: """
+
+    
+    try:
+        a = int(input(metin))
+        if a > 17 or a < 1:
+            print("Please enter between 1-17 only !")
+        elif a == 17:
+            print("Calculator has been closed.")                                                                                                                    
+        elif a == 16:
+            main()
+                
+        else:
+            if a == 1:
+                savasci = "AseraiRecruit"
+            if a == 2:
+                savasci = "AseraiTribesman"
+            if a == 3:
+                savasci = "AseraiSkirmisher"
+            if a == 4:
+                savasci = "AseraiArcher"
+            if a == 5:
+                savasci = "AseraiMasterArcher"
+            if a == 6:
+                savasci = "AseraiFootman"
+            if a == 7:
+                savasci = "AseraiInfantry"
+            if a == 8:
+                savasci = "AseraiVeteranInfantry"
+            if a == 9:
+                savasci = "AseraiMamelukeSoldier"
+            if a == 10:
+                savasci = "AseraiMamelukeRegular"
+            if a == 11:
+                savasci = "AseraiMamelukeCavalry"
+            if a == 12:
+                savasci = "AseraiMamelukeHeavyCavalry"
+            if a == 13:
+                savasci = "AseraiMamelukeAxeman"
+            if a == 14:
+                savasci = "AseraiMamelukeGuard"
+            if a == 15:
+                savasci = "AseraiMamelukePalaceGuard"
+            b = int(input("How many?: "))
+            getPrice(savasci, b)
+    except ValueError:
+        print("Only numbers, please!")
+
+def sturgia():
+    metin = """
+    1) Sturgian Recruit
+    2) Sturgian Warrior
+    3) Sturgian Soldier
+    4) Sturgian Spearman
+    5) Sturgian ShockTroop
+    6) Sturgian Veteran Warrior
+    7) Sturgian Berserker
+    8) Sturgian Ulfhednar
+    9) Sturgian Woodsman
+    10) Sturgian Brigand
+    11) Sturgian Hunter
+    12) Sturgian Hardened Brigand
+    13) Sturgian Horse Raider
+    14) Sturgian Archer
+    15) Sturgian Veteran Bowman
+
+    16) Change Kingdom
+    17) Exit
+
+    Which one do you want ?: """
+
+    
+    try:
+        a = int(input(metin))
+        if a > 17 or a < 1:
+            print("Please enter between 1-17 only !")
+        elif a == 17:
+            print("Calculator has been closed.")                                                                                                                    
+        elif a == 16:
+            main()
+                
+        else:
+            if a == 1:
+                savasci = "SturgianRecruit"
+            if a == 2:
+                savasci = "SturgianWarrior"
+            if a == 3:
+                savasci = "SturgianSoldier"
+            if a == 4:
+                savasci = "SturgianSpearman"
+            if a == 5:
+                savasci = "SturgianShockTroop"
+            if a == 6:
+                savasci = "SturgianVeteranWarrior"
+            if a == 7:
+                savasci = "SturgianBerserker"
+            if a == 8:
+                savasci = "SturgianUlfhednar"
+            if a == 9:
+                savasci = "SturgianWoodsman"
+            if a == 10:
+                savasci = "SturgianBrigand"
+            if a == 11:
+                savasci = "SturgianHunter"
+            if a == 12:
+                savasci = "SturgianHardenedBrigand"
+            if a == 13:
+                savasci = "SturgianHorseRaider"
+            if a == 14:
+                savasci = "SturgianArcher"
+            if a == 15:
+                savasci = "SturgianVeteranBowman"
+            b = int(input("How many?: "))
+            getPrice(savasci, b)
+    except ValueError:
+        print("Only numbers, please!")
 
 def vlandian():
     metin = """
@@ -128,19 +390,19 @@ def vlandian():
     14) Vlandian Swordsman
     15) Vlandian Sergeant
 
-    16) Ülke Değiştir
-    17) Çıkış
+    16) Change Kingdom
+    17) Exit
 
-    Mevcut Asker Rütbesi Nedir? """
+    Which one do you want?: """
 
     
     try:
         a = int(input(metin))
         if a > 17 or a < 1:
-            print("Lütfen 1-17 Arası İşlem Giriniz!")
+            print("Please enter between 1-17 only !")
         elif a == 17:
-            print("Program Sonlandırıldı!")                                                                                                                    
-        elif a == 17:
+            print("Calculator has been closed.")                                                                                                                    
+        elif a == 16:
             main()
                 
         else:
@@ -174,10 +436,10 @@ def vlandian():
                 savasci = "VlandianSwordsman"
             if a == 15:
                 savasci = "VlandianSergeant"
-            b = int(input("Adet Giriniz: "))
+            b = int(input("How many?: "))
             getPrice(savasci, b)
     except ValueError:
-        print("Lütfen sadece sayı giriniz.")
+        print("Only numbers, please!")
     
     
     
@@ -200,18 +462,18 @@ def khuzait():
     14) Khuzait Spear Infantry
     15) Khuzait Darkhan
 
-    16) Ülke Değiştir
-    17) Çıkış
+    16) Change Kingdom
+    17) Exit
 
-    Mevcut Asker Rütbesi Nedir? """
+    Which one do you want?: """
 
     try:
         a = int(input(metin))
         if a > 17 or a < 1:
-            print("Lütfen 1-17 Arası İşlem Giriniz!")
+            print("Please enter between 1-17 only !")
         elif a == 17:
-            print("Program Sonlandırıldı!")                                                                                                                    
-        elif a == 17:
+            print("Calculator has been closed.")                                                                                                                    
+        elif a == 16:
             main()
                 
         else:
@@ -245,10 +507,10 @@ def khuzait():
                 savasci = "KhuzaitSpearInfantry"
             if a == 15:
                 savasci = "KhuzaitDarkhan"
-            b = int(input("Adet Giriniz: "))
+            b = int(input("How many?: "))
             getPrice(savasci, b)
     except ValueError:
-        print("Lütfen sadece sayı giriniz.")
+        print("Only numbers, please!")
         
 
 def main():
@@ -256,22 +518,31 @@ def main():
 1) Khuzait
 2) Vlandian
 3) Sturgia
+4) Aserai
+5) Imperial
+6) Battanian
 
-7) Çıkış
+7) Exit
 """)
     try:
-        a = int(input("Ülke Seçiniz: "))
+        a = int(input("Choose: "))
         if a >= 1 or a <= 7:
             if a == 1:
                 khuzait()
             if a == 2:
                 vlandian()
             if a == 3:
-                sturgia()            
+                sturgia()        
+            if a == 4:
+                aserai()    
+            if a == 5:
+                imperial()
+            if a == 6:
+                battanian()
             if a == 7:
-                print("Program Sonlandırıldı!")
+                print("Calculator has been closed.")
                 return
     except ValueError:
-        print("Lütfen Sadece Rakam  Giriniz")
+        print("Enter only numbers, please!")
 
 main()
